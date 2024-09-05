3001727059fbc31684ca280e4ec66444f8890c11
verdon-helene-emf <helene.verdon@studentfr.ch>
2024-09-05 11:18:30 +0200

    IfSwitchGithub

---
+++ ExempleIfSwitch/IfSwitch.java
@@ -0,0 +1,9 @@
+package ExempleIfSwitch;
+
+public class IfSwitch {
+    public static void main(String[] args) {
+        int note =5;
+
+        System.out.println("Ma note est :" + note);
+    }
+}
