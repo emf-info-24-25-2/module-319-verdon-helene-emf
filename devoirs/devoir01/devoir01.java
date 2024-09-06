42dedf5a8e075b5d365e9790f840f1c6e5a4748f
verdon-helene-emf <helene.verdon@studentfr.ch>
2024-09-06 21:10:11 +0200

    devoir01

--- devoirs/devoir01/devoir01.java
+++ devoirs/devoir01/devoir01.java
@@ -1,29 +1,21 @@
-
 public class devoir01 {
+
+  public static void main(String[] args) {
+
+    byte monAge = 16;
+    boolean estMajeur = (monAge > 20);
+
+    System.out.println("Je m'appelle Hélène Verdon");
+    System.out.println("Mon âge est de " + monAge + " ans.");
+
+    if (estMajeur) {
+      System.out.println("Je suis majeure.");
     
-    public static void main(String[] args) {
+    }
 
-      byte monAge = 16;
-      
-      boolean estMajeur;
-
-      if (estMajeur > 20) {
-        estMajeur = true;
-        }
-
-        else {
-        estMajeur = false;
-      }
-      
-      System.out.println("Je m'appelle VERDON hélène.");
-
-      System.out.println("Mon âge est de " +monAge "ans.");
-
-      if (estMajeur = True) {
-        System.out.println("Je suis majeur.");
-      }
-      
-      else {
-        System.out.println("Je ne suis pas encore majeure.");
-      } 
+    else {
+      System.out.println("Je ne suis pas encore majeure.");
+    
+    }
+  }
 }
