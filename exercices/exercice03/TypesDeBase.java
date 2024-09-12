0c852c2201ca178599bfe606b0447c01425b6599
verdon-helene-emf <helene.verdon@studentfr.ch>
2024-09-12 09:12:18 +0200

    ex

---
+++ exercices/exercice03/TypesDeBase.java
@@ -0,0 +1,14 @@
+import javax.sound.sampled.SourceDataLine;
+
+public class TypesDeBase {
+    public static void main(String[] args) {
+        
+    
+    Byte age = 16;
+    String nom = "Hélène";
+
+    System.out.println("Je m'appelle " + nom + " et j'ai " + age + " ans");
+
+    }
+
+}
