bc6b86124ff5b109a59ab7226d09ea3ca3badcfe
verdon-helene-emf <helene.verdon@studentfr.ch>
2024-09-19 11:18:59 +0200

    ex

--- exercices/exercice09/ExerciceTableaux2.java
+++ exercices/exercice09/ExerciceTableaux2.java
@@ -1,6 +1,7 @@
 public class ExerciceTableaux2 {
    
-   
+   public final static int VALEUR_MAX = 6;
+   public final static int VALEUR_MIN = 0;
    
     public static void main(String[] args) {
         
@@ -8,12 +9,12 @@ public class ExerciceTableaux2 {
        int [] tableau1;
        tableau1 = new int[10];
 
-       //assigner la valeur aléatoire de la constante à chaque cellule
-        for (int a = 0; a<10; a++) {
-            int valeurAleatoire = (int) (Math.random() * (6 - 0 + 1)) + 0;
+       //assigner la valeur aléatoire différente à chaque cellule
+        for (int a = 0; a<tableau1.length; a++) {
+            int valeurAleatoire = (int) (Math.random() * (VALEUR_MAX - VALEUR_MIN + 1)) + VALEUR_MIN;
             tableau1[a] = valeurAleatoire;
         }
-        for (int b = 0; b<10; b++){
+        for (int b = 0; b<tableau1.length; b++){
             System.out.println("Cellule " + b + " : " + tableau1[b]);
         }
 
