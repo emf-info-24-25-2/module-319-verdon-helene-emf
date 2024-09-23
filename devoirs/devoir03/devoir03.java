bcb5b287691c35d7ca7d1f58666c636cd2c0b48f
verdon-helene-emf <helene.verdon@studentfr.ch>
2024-09-23 20:00:13 +0200

    ex

--- devoirs/devoir03/devoir03.java
+++ devoirs/devoir03/devoir03.java
@@ -1,8 +1,7 @@
 public class devoir03 {
     public static void main(String[] args) {
 
-        // déclaration des variables
-        int jour;
+        /*
         int mois;
         int annee;
         int numeroDuJour;
@@ -12,7 +11,7 @@ public class devoir03 {
 
         int nombreJourParMois;
 
-        // assignation de ces variables
+   
         jour = 23;
         mois = 9;
         annee = 2024;
@@ -20,29 +19,95 @@ public class devoir03 {
         numeroDuJour = 0;
         moisPair = 30;
         moisImpair = 31;
-        fevrier = 28;
+    
 
-        // déterminer que si le mois est janvier le numero du jour est egal au jour (par
-        // exemple le 21 janvier est le jour 21 de l'an)
-        
-            if (annee % 4 == 0) {
-               
-               
-                if (mois-1 != 0) {
+      
+            if (mois-1 != 0) {
 
-                    if ((mois-1) % 2 == 0) {
-                        numeroDuJour = (((mois - 1) / 2) * moisPair) + (((mois - 1) / 2) * moisImpair) + (jour);
-                    } else {
-                        numeroDuJour = (((mois - 2) / 2) * moisPair) + (((mois - 2) / 2) * moisImpair) + (moisImpair) + (jour);
-                    }
-                    System.out.println(numeroDuJour);
+                if (mois > 2) {
+
+                        if ((mois-1) % 2 == 0) {
+                            numeroDuJour = (((mois - 1) / 2) * moisPair) + (((mois - 1) / 2) * moisImpair) + (jour);
+                        } else {
+                            numeroDuJour = (((mois - 2) / 2) * moisPair) + (((mois - 2) / 2) * moisImpair) + (moisImpair) + (jour);
+                        }
+                        System.out.println(numeroDuJour);
 
                 } else {
                     System.out.println(jour);
                 }
+            
             }
 
-       
+        */
 
+
+
+
+
+
+
+
+
+
+
+
+            int jour;
+            int mois;
+            int annee;
+            int numeroDuJour;
+            int moisPair;
+            int moisImpair;
+            int fevrier;
+    
+            // assignation des variables
+            jour = 23;
+            mois = 9;
+            annee = 2024;
+    
+            numeroDuJour = 0;
+            moisPair = 30;
+            moisImpair = 31;
+    
+            // Vérifier si l'année est bissextile
+            if ((annee % 4 == 0 && annee % 100 != 0) || (annee % 400 == 0)) {
+                fevrier = 29;
+            } else {
+                fevrier = 28;
+            }
+    
+            // Calcul du nombre de jours écoulés avant le mois donné
+            switch (mois) {
+                case 1:
+                    numeroDuJour = jour; // Janvier
+                    break;
+                case 2:
+                    numeroDuJour = moisImpair + jour; // Janvier + février
+                    break;
+                case 3:
+                    numeroDuJour = moisImpair + fevrier + jour; // Janvier + février + mars
+                    break;
+                case 4:
+                    numeroDuJour = moisImpair + fevrier + moisImpair + jour; // Jusqu'à avril
+                    break;
+                case 5:
+                    numeroDuJour = moisImpair + fevrier + moisImpair + moisPair + jour; // Jusqu'à mai
+                    break;
+                case 6:
+                    numeroDuJour = moisImpair + fevrier + moisImpair + moisPair + moisImpair + jour;
+                    break;
+                case 7:
+                    numeroDuJour = moisImpair + fevrier + moisImpair + moisPair + moisImpair + moisPair + jour;
+                    break;
+                case 8:
+                    numeroDuJour = moisImpair + fevrier + moisImpair + moisPair + moisImpair + moisPair + moisImpair + jour;
+                    break;
+                case 9:
+                    numeroDuJour = moisImpair + fevrier + moisImpair + moisPair + moisImpair + moisPair + moisImpair + moisImpair + jour;
+                    break;
+            }
+    
+            System.out.println("Le " + jour + "/" + mois + "/" + annee + " est le jour " + numeroDuJour + " de l'An.");
+        }
     }
-}
+
