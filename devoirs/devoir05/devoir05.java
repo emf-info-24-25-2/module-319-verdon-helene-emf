40722a665d7568e75e7e5935e592056d898b6cbc
verdon-helene-emf <helene.verdon@studentfr.ch>
2024-10-03 09:59:16 +0200

    dev05

---
+++ devoirs/devoir05/devoir05.java
@@ -0,0 +1,23 @@
+package devoirs.devoir05;
+
+//importer le scanner
+import java.util.Scanner;
+
+public class devoir05 {
+    public static void main(String[] args) {
+        
+        //déclarer le scanner
+        Scanner scanner = new Scanner(System.in);
+
+        //faire choisir à l'ordinateur un nombre aléatoire entre 1 et 100
+
+
+        //dire au joueur que l'ordinateur a choisi un nombre entre 1 et 10
+
+        //dire au joueur de deviner le nombrer et de l'entrer au clavier
+
+    
+}
+
+
+}
