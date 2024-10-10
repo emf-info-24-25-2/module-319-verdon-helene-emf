5e50f9c7f5bc5668ab9fbbe8b813c842629da054
verdon-helene-emf <helene.verdon@studentfr.ch>
2024-10-10 11:44:19 +0200

    dev

--- devoirs/devoir03/devoir03.java
+++ devoirs/devoir03/devoir03.java
@@ -1,52 +1,12 @@
 public class devoir03 {
     public static void main(String[] args) {
 
-        /* j'avais essayé quelque chose car je voulais essayer avec une boucle au lieu d'un switch mais (j'ai raté masi ne voulais pas tout supprimer)
-        int mois;
-        int annee;
-        int numeroDuJour;
-        int moisPair;
-        int moisImpair;
-        int fevrier;
-
-        int nombreJourParMois;
-
-   
-        jour = 23;
-        mois = 9;
-        annee = 2024;
-
-        numeroDuJour = 0;
-        moisPair = 30;
-        moisImpair = 31;
-    
-
-      
-            if (mois-1 != 0) {
-
-                if (mois > 2) {
-
-                        if ((mois-1) % 2 == 0) {
-                            numeroDuJour = (((mois - 1) / 2) * moisPair) + (((mois - 1) / 2) * moisImpair) + (jour);
-                        } else {
-                            numeroDuJour = (((mois - 2) / 2) * moisPair) + (((mois - 2) / 2) * moisImpair) + (moisImpair) + (jour);
-                        }
-                        System.out.println(numeroDuJour);
-
-                } else {
-                    System.out.println(jour);
-                }
-            
-            }
-
-        */
-
             int jour;
             int mois;
             int annee;
             int numeroDuJour;
-            int moisPair;
-            int moisImpair;
+            int moisCourt;
+            int moisLong;
             int fevrier;
     
             // assignation des  variables.
@@ -55,8 +15,8 @@ public class devoir03 {
             annee = 2024;
     
             numeroDuJour = 0;
-            moisPair = 30;
-            moisImpair = 31;
+            moisCourt = 30;
+            moisLong = 31;
     
             // Vérifier si l'année est bissextile
             if ((annee % 4 == 0 && annee % 100 != 0) || (annee % 400 == 0)) {
@@ -71,32 +31,55 @@ public class devoir03 {
                     numeroDuJour = jour; // Janvier
                     break;
                 case 2:
-                    numeroDuJour = moisImpair + jour; // Janvier + février
+                    numeroDuJour = moisLong + jour; // Janvier + février
                     break;
                 case 3:
-                    numeroDuJour = moisImpair + fevrier + jour; // Janvier + février + mars
+                    numeroDuJour = moisLong + fevrier + jour; // Janvier + février + mars
                     break;
                 case 4:
-                    numeroDuJour = moisImpair + fevrier + moisImpair + jour; // Jusqu'à avril
+                    numeroDuJour = moisLong + fevrier + moisLong + jour; // Jusqu'à avril
                     break;
                 case 5:
-                    numeroDuJour = moisImpair + fevrier + moisImpair + moisPair + jour; // Jusqu'à mai
+                    numeroDuJour = moisLong + fevrier + moisLong + moisCourt + jour; // Jusqu'à mai
                     break;
                 case 6:
-                    numeroDuJour = moisImpair + fevrier + moisImpair + moisPair + moisImpair + jour;
+                    numeroDuJour = moisLong + fevrier + moisLong + moisCourt + moisLong + jour;
                     break;
                 case 7:
-                    numeroDuJour = moisImpair + fevrier + moisImpair + moisPair + moisImpair + moisPair + jour;
+                    numeroDuJour = moisLong + fevrier + moisLong + moisCourt + moisLong + moisCourt + jour;
                     break;
                 case 8:
-                    numeroDuJour = moisImpair + fevrier + moisImpair + moisPair + moisImpair + moisPair + moisImpair + jour;
+                    numeroDuJour = moisLong + fevrier + moisLong + moisCourt + moisLong + moisCourt + moisLong + jour;
                     break;
                 case 9:
-                    numeroDuJour = moisImpair + fevrier + moisImpair + moisPair + moisImpair + moisPair + moisImpair + moisImpair + jour;
+                    numeroDuJour = moisLong + fevrier + moisLong + moisCourt + moisLong + moisCourt + moisLong + moisLong + jour;
                     break;
             }
     
             System.out.println("Le " + jour + "/" + mois + "/" + annee + " est le jour " + numeroDuJour + " de l'An.");
+
+
+
+            /* 
+            int jour = 12;
+            int mois = 10;
+            int annee = 2024;
+
+            // Vérifier si l'année est bissextile
+            int fevrier = (annee % 4 == 0 && annee % 100 != 0) || (annee % 400 == 0) ? 29 : 28;
+            
+            // Jours dans chaque mois
+            int[] joursParMois = {0, 31, fevrier, 31, 30, 31, 30, 31, 31, 30, 31, 30};
+
+            // Calcul du nombre de jours écoulés avant le mois donné
+            int numeroDuJour = jour; // Jours du mois en cours
+
+            for (int i = 1; i < mois; i++) {
+                numeroDuJour += joursParMois[i]; // Ajoute les jours des mois précédents
+            }
+
+            System.out.println("Le " + jour + "/" + mois + "/" + annee + " est le jour " + numeroDuJour + " de l'An.");
+            */
         }
     }
 
