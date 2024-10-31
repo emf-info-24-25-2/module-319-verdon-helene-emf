14ae38a5c6bb4603d6cf42c6b10fab0a1827d6d5
verdon-helene-emf <helene.verdon@studentfr.ch>
2024-10-31 10:34:46 +0100

    methodses tableaux ex 10

--- exercices/exercice10/ExercicesMethodesTableaux/src/methodesTableaux.java
+++ exercices/exercice10/ExercicesMethodesTableaux/src/methodesTableaux.java
@@ -1,26 +1,34 @@
-public class methodesTableaux {
-    public static void main(String[] args) {
-        int[] tableauMain = creerTableau(5);
-        for(int j = 0; j < tableauMain.length; j++) {
-            System.out.println(tableauMain[j]);
-        }
 
-        remplirTableau(tableauMain, 15);
-        for(int k = 0; k < tableauMain.length; k++) {
-            System.out.println(tableauMain[k]);
-        }
+import java.util.Random;
+
+public class methodesTableaux {
+
+    public static void main(String[] args) {
+      int[] tableauMain = creerTableau(5);
+      for(int j = 0; j < tableauMain.length; j++) {
+        System.out.println(tableauMain[j]);
+      }
+
+      remplirTableau(tableauMain, 15);
+      for(int k = 0; k < tableauMain.length; k++) {
+        System.out.println(tableauMain[k]);
+      }
+      remplirTableauAleatoire(tableauMain);
+      for(int l = 0; l < tableauMain.length; l++) {
+        System.out.println(tableauMain[l]);
+      }
     }
 
 
 
 
-
+    //1
     public static int[] creerTableau(int taille) {
         int[] monTableau = new int[taille];
         return monTableau;
     }
 
-
+    //2
     public static int[] remplirTableau(int[] tableauMain, int valeur) {
         for(int i = 0; i < tableauMain.length; i++) {
             tableauMain[i] = valeur;
@@ -28,5 +36,15 @@ public class methodesTableaux {
         return tableauMain;
     }
 
+    //3
+    public static int[] remplirTableauAleatoire(int[] tableauMain) {
+        for (int i = 0; i < tableauMain.length; i++) {
+            
+            int valeurAleatoire = new Random().nextInt(100);
+            tableauMain[i] = valeurAleatoire;
+        }
+        return tableauMain;
+    }
 }
+
 
