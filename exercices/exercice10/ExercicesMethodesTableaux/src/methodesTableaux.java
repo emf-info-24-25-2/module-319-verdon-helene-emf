728feb3d3f87545c1189e28ee259fb9cdb380947
verdon-helene-emf <helene.verdon@studentfr.ch>
2024-11-07 09:56:03 +0100

    s

--- exercices/exercice10/ExercicesMethodesTableaux/src/methodesTableaux.java
+++ exercices/exercice10/ExercicesMethodesTableaux/src/methodesTableaux.java
@@ -2,49 +2,52 @@ import java.util.Random;
 
 public class methodesTableaux {
     public static void main(String[] args) {
-      System.out.println("Exercice 1 :");
-      int[] tableauMain = creerTableau(5);
+      System.out.println("Exercice 1, Créer un tableau d'entiers d'un certain nombre de cellules:");
+      int tailleTableau = 5;
+      int[] tableauMain = creerTableau(tailleTableau);
+      System.out.println("Le tableau a une taille de " + tailleTableau + " cellules.");
       for(int j = 0; j < tableauMain.length; j++) {
         System.out.println(tableauMain[j]);
       }
+      
 
-      System.out.println("Exercice 2 :");
+      System.out.println("Exercice 2, Remplir un tableau avec une valeur fixe. :");
       remplirTableau(tableauMain, 15);
       for(int k = 0; k < tableauMain.length; k++) {
         System.out.println(tableauMain[k]);
       }
 
-      System.out.println("Exercice 3 :");
+      System.out.println("Exercice 3, Remplir un tableau avec des valeurs aléatoires.  :");
       remplirTableauAleatoire(tableauMain);
       for(int l = 0; l < tableauMain.length; l++) {
         System.out.println(tableauMain[l]);
       }
 
-      System.out.println("Exercice 4 :");
+      System.out.println("Exercice 4, Déterminer la taille d'un tableau :");
       obtenirTaille(tableauMain);
       System.out.println("Voici la taille du tableau : " + obtenirTaille(tableauMain));
       
-      System.out.println("Exercice 5 :");
+      System.out.println("Exercice 5, Afficher le contenu d'un tableau  :");
       contenuTableau(tableauMain);
 
-      System.out.println("Exercice 6 :");
+      System.out.println("Exercice 6, Rechercher la valeur minimum d'un tableau  :");
       trouverMinimum(tableauMain);
 
-      System.out.println("Exercice 7 :");
+      System.out.println("Exercice 7, Rechercher la valeur maximum d'un tableau  :");
       trouverMaximum(tableauMain);
 
-      System.out.println("Exercice 8 :");
+      System.out.println("Exercice 8, Compter la fréquence d'une valeur dans un tableau.  :");
       int valeur = 15;
       System.out.println("La valeur " + valeur + " se retrouve " +  trouverFrequence(tableauMain,valeur) + " fois dans le tableau.");
 
-      System.out.println("Exercice 9 :");
+      System.out.println("Exercice 9, Etablir la somme des valeurs contenues dans un tableau.  :");
       System.out.println("Somme de toutes les valeurs du tableau : " + trouverSomme(tableauMain));
 
-      System.out.println("Exercice 10 :");
+      System.out.println("Exercice 10, Etablir la moyenne des valeurs contenues dans un tableau. :");
       double moyenneTableau = trouverMoyennes(tableauMain);
       System.out.println("La moyenne du tableau est de : " + moyenneTableau);
 
-      System.out.println("Exercice 11 :");
+      System.out.println("Exercice 11, Remplacer une valeur par une autre dans un tableau  :");
       for (int i = 0; i < tableauMain.length; i++) {
         System.out.println(tableauMain[i]);
       }
@@ -60,13 +63,22 @@ public class methodesTableaux {
         System.out.println(tableauMain[i]);
       }
 
-      System.out.println("Exercice 12 :");
+      System.out.println("Exercice 12, Rechercher la première occurrence d'une valeur dans un tableau  :");
       int nbrRecherche = 16;
       int premiereOccurence = recherchePremiereOcurrence(tableauMain, nbrRecherche);
       if (premiereOccurence >= 0) { 
         System.out.println("La première occurence du nombre " + nbrRecherche + " dans le tableau est dans la cellule numéro " + premiereOccurence );
       } else {
         System.out.println("Le nombre " + nbrRecherche + " n'apparaît pas dans le tableau");
+      }
+
+      System.out.println("Exercice 13, Rechercher la dernière occurrence d'une valeur dans un tableau  :");
+      int nbrRecherche2 = 27;
+      int derniereOccurence = rechercheDerniereOcurrence(tableauMain, nbrRecherche2);
+      if (derniereOccurence < tableauMain.length) { 
+        System.out.println("La dernière occurence du nombre " + nbrRecherche2 + " dans le tableau est dans la cellule numéro " + derniereOccurence );
+      } else {
+        System.out.println("Le nombre " + nbrRecherche2 + " n'apparaît pas dans le tableau");
       }
     }
 
@@ -191,6 +203,18 @@ public class methodesTableaux {
         return premiereOccurence;
     }
 
+
+    //13 Rechercher la dernière occurence d'une valeur d'un tableau
+    public static int rechercheDerniereOcurrence (int[]tableauMain, int valeurRechercher) {
+        int derniereOccurence = tableauMain.length;
+        for (int i = tableauMain.length; i <= tableauMain.length -1; i--) {
+            if (tableauMain[i] == valeurRechercher) {
+                derniereOccurence = i;
+                break;
+            }
+        }
+        return derniereOccurence;
+    }
 }
 
 
