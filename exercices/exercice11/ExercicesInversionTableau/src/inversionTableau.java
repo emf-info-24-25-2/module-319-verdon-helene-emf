aac81c4b14dbd9545e7cc45c42675a6d1fb11110
verdon-helene-emf <helene.verdon@studentfr.ch>
2024-11-07 10:49:33 +0100

    sad

--- exercices/exercice11/ExercicesInversionTableau/src/inversionTableau.java
+++ exercices/exercice11/ExercicesInversionTableau/src/inversionTableau.java
@@ -1,12 +1,31 @@
 public class inversionTableau {
-    
+    public final static int MIN = 0;
+    public final static int MAX = 100; 
     public static void main(String[] args) {
-        int[] tableau = {1, 2, 3, 4, 5};
-        int[] tableauInverse = inverseLeTableau(tableau);
+        System.out.println("Premier tableau Initial");
+        int[] tableauInitial1 = {1, 2, 3, 4, 5};
+        int[] tableauInverse;
+        tableauInverse = inverseLeTableau(tableauInitial1);
 
-        System.out.println("Tableau inversé : ");
+        System.out.println("Premier tableau inversé : ");
         for (int i = 0; i < tableauInverse.length; i++) {
             System.out.println(tableauInverse[i]);
+        }
+
+       
+        System.out.println("Deuième tableau initial");
+        int[] tableauInitial2;
+        tableauInitial2 = new int[4];
+        for (int i = 0; i < tableauInitial2.length; i++) {
+            tableauInitial2[i] = ( int ) (Math.random() * (MAX - MIN) ) + MIN;
+            System.out.println(tableauInitial2[i]);
+        }
+
+        System.out.println("Deuxième tableau inversé : ");
+        int[] tableauFinal;
+        tableauFinal = inverseLeTableau(tableauInitial2);
+        for (int j = 0; j < tableauFinal.length; j++) {
+            System.out.println(tableauFinal[j]);
         }
     }
     
@@ -23,4 +42,6 @@ public class inversionTableau {
         return tableauInverse;
 
     }
+
+
 }
