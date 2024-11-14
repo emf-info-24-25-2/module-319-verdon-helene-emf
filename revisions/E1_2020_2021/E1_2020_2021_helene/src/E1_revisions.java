adcaf69d6c945ff9eb6157ed912676230c0e9698
verdon-helene-emf <helene.verdon@studentfr.ch>
2024-11-14 09:58:10 +0100

    E1_corrigée

--- revisions/E1_2020_2021/E1_2020_2021_helene/src/E1_revisions.java
+++ revisions/E1_2020_2021/E1_2020_2021_helene/src/E1_revisions.java
@@ -1,5 +1,5 @@
 public class E1_revisions {
-  
+
     public final static int LIBRE = 0;
     public final static int OCCUPEE = 1;
     public final static int TOUCHEE = 2;
@@ -7,68 +7,72 @@ public class E1_revisions {
     public final static int TAILLE_GRILLE = 20;
     public final static int NBRE_BATEAUX = 5;
 
-    // methode pour choisir un nombre aleatoire entre 1 et le dernier index de la grille
+    // methode pour choisir un nombre aleatoire entre 1 et le dernier index de la
+    // grille
     public static int choisirIndexAleatoire(int min, int max) {
-        int aleatoire = ( int ) ( Math.random() * ( max - min + 1 ) ) + min;
+        int aleatoire = (int) (Math.random() * (max - min + 1)) + min;
         return aleatoire;
     }
 
-    // methode pour générer une grille et tant que tous les bateaux a placer ne le sont pas, elle choisit un nouvel index aleatoire
+    // methode pour générer une grille et tant que tous les bateaux a placer ne le
+    // sont pas, elle choisit un nouvel index aleatoire
     public static int[] gernererGrille(int tailleGrille, int nbreBateaux) {
         int[] grille = new int[tailleGrille];
         int nbreBateauxPlaces = 0;
-        int min = 1;
-        int max = tailleGrille--;
+        int min = 0;
+        int max = tailleGrille - 1;
+
         while (nbreBateauxPlaces < nbreBateaux) {
             int aleatoire = choisirIndexAleatoire(min, max);
             if (grille[aleatoire] == LIBRE) {
                 grille[aleatoire] = OCCUPEE;
-                nbreBateauxPlaces ++;
+                nbreBateauxPlaces++;
             }
         }
         return grille;
     }
 
-    public static boolean testerGrille(int[]tableau) {
+    // methode qui test si le joueur a touché toutes les bateaux et si non renvoie
+    // false catr le jeu n'est pas terminé
+    public static boolean testerGrille(int[] tableau) {
         int nbreIndexOccupes = 0;
         boolean testSiTermine = true;
-        for (int i = 0; 0 < tableau.length; i++) {
+        for (int i = 0; i < tableau.length; i++) {
             if (tableau[i] == OCCUPEE) {
-                nbreIndexOccupes ++;
+                nbreIndexOccupes++;
             }
         }
         if (nbreIndexOccupes > 0) {
             testSiTermine = false;
         }
         return testSiTermine;
-        }
-
+    }
 
     public static void main(String[] args) {
         boolean jeuTermine = false;
         int nbreCoups = 0;
         int[] grille = gernererGrille(TAILLE_GRILLE, NBRE_BATEAUX);
-        boolean terminer = testerGrille(grille);
         int min = 1;
-        int max = TAILLE_GRILLE-1;
-        while (terminer == false) {
+        int max = TAILLE_GRILLE - 1;
+        while (jeuTermine == false) {
             int aleatoire = choisirIndexAleatoire(min, max);
             int valeurCelluleVisee = grille[aleatoire];
-            int celluleVisee = aleatoire;
             nbreCoups++;
-            System.out.println("Le joueur vise la cellule " + celluleVisee + "...");
+            System.out.println("Le joueur vise la cellule " + aleatoire + "...");
 
             switch (valeurCelluleVisee) {
-                case LIBRE :
-                System.out.println("A l'eau");
-                case OCCUPEE :
-                System.out.println("Touché");
-                grille[aleatoire] = TOUCHEE;
-                case TOUCHEE :
-                System.out.println("Deja touchée...");
+                case LIBRE:
+                    System.out.println("A l'eau");
+                    break;
+                case OCCUPEE:
+                    System.out.println("Touché");
+                    grille[aleatoire] = TOUCHEE;
+                    break;
+                case TOUCHEE:
+                    System.out.println("Deja touchée...");
             }
 
-            if (jeuTermine == true) {
+            if (testerGrille(grille) == true) {
                 System.out.println("Le jeu est terminé en " + nbreCoups + " coups.");
                 jeuTermine = true;
             }
