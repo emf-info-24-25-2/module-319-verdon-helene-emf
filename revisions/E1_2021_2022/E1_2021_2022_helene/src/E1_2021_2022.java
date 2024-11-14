1ae0bb2a048fb9060bad9a7890d32e443a128872
verdon-helene-emf <helene.verdon@studentfr.ch>
2024-11-14 11:44:16 +0100

    afds

--- revisions/E1_2021_2022/E1_2021_2022_helene/src/E1_2021_2022.java
+++ revisions/E1_2021_2022/E1_2021_2022_helene/src/E1_2021_2022.java
@@ -19,21 +19,40 @@ public class E1_2021_2022 {
     }
     
     public static String donnerNomCarte(int carte) {
-        if ((carte > 2) && (carte < 10)) {
-            
+        String nom;
+        if ((carte >= 2) && (carte <= 10)) {
+            nom = String.valueOf(carte)
+        } else {
+            switch (carte) {
+                case 11:
+                    nom = "valet";
+                    break;
+                case 12:
+                nom = "dame";
+                    break;
+                case 13 :
+                nom = "roi";
+                    break;
+                case 14:
+                nom = "as";
+                    break;
+                default :
+                nom = "indeterminé";
+
+            }
         }
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
     public static void main(String[] args) throws Exception {
+        String[] joueurs = {"Jacques", "Pierre", "Paul"};
+        int[] scores = new int[3];
+        boolean jeuTermine = false;
+        while (jeuTermine == false) {
+            for (int i = 0; i < joueurs.length; i++) {
+                int[] carte = new int[joueurs.length];
+                carte[i] = tirerUneCarte(MIN, MAX);
+            }
+        }
         
     }
 }
